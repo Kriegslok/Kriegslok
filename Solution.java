70ecc9a0d32380eba36068164e1731057f592e4d
Kriegslok <63340342+Kriegslok@users.noreply.github.com>
2020-11-04 09:57:29 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Equals and HashCode
+*/
+
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Solution n) {
+        boolean bul = n.first == first && n.last == last;
+        System.out.println(bul);
+        return bul;///iikkij
+    }
+
+    public int hashCode() {
+
+        return 31 * first.hashCode() + last.hashCode();
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
+}
